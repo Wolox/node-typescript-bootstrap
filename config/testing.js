d3743ef7a55e97b333ee831b6b622f1ef88c4726
Martín Picollo <martin.picollo@wolox.com.ar>
2019-05-14 22:39:12 -0300

    Kickoff project

---
+++ config/testing.js
@@ -0,0 +1,13 @@
+exports.config = {
+  environment: 'testing',
+  isTesting: true,
+  common: {
+    database: {
+      name: process.env.DB_NAME_TEST
+    },
+
+    session: {
+      secret: 'some-super-secret'
+    }
+  }
+};
