06477a67a121005a295ece342f84340f0c18ff1b
bhflm <germanbonin@gmail.com>
2019-07-10 22:06:18 -0300

    (refactor): middlewares/error.ts

--- app/middlewares/errors.ts
+++ app/middlewares/errors.ts
@@ -1,3 +1,5 @@
+export = { };
+
 const errors = require('../errors'),
   logger = require('../logger');
 
@@ -9,7 +11,7 @@ const statusCodes = {
   [errors.NOT_FOUND]: 404
 };
 
-exports.handle = (error, _, res, next) => {
+exports.handle = (error: { internalCode: string | number; message: any; }, _: any, res: { status: { (arg0: number): void; (arg0: number): void; }; send: (arg0: { message: any; internal_code: any; }) => void; }, next: (arg0: any) => void) => {
   if (error.internalCode) {
     res.status(statusCodes[error.internalCode] || DEFAULT_STATUS_CODE);
   } else {
