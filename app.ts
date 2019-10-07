87f6bdf8f5466af7df27086edd931f684ae58910
Emanuel Casco <cascoemanuel@gmail.com>
2019-10-07 14:57:32 -0300

    Fix body-parser options in app.js

--- app.ts
+++ app.ts
@@ -1,26 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import express from 'express';
-import bodyParser from 'body-parser';
+import bodyParser, { OptionsJson, OptionsUrlencoded } from 'body-parser';
 import path from 'path';
 import cors from 'cors';
 import config from './config';
 import * as routes from './app/routes';
 import * as errors from './app/middlewares/errors';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { expressMiddleware, expressRequestIdMiddleware } = require('express-wolox-logger');
 
 const DEFAULT_BODY_SIZE_LIMIT = 1024 * 1024 * 10;
 const DEFAULT_PARAMETER_LIMIT = 10000;
 
-const bodyParserJsonConfig = () => ({
-  parameterLimit: config.common.api.parameterLimit || DEFAULT_PARAMETER_LIMIT,
-  limit: config.common.api.bodySizeLimit || DEFAULT_BODY_SIZE_LIMIT
-});
+const bodyParserJsonConfig: OptionsJson = {
+  limit: Number(config.common.api.bodySizeLimit) || DEFAULT_BODY_SIZE_LIMIT
+};
 
-const bodyParserUrlencodedConfig = () => ({
+const bodyParserUrlencodedConfig: OptionsUrlencoded = {
   extended: true,
-  parameterLimit: config.common.api.parameterLimit || DEFAULT_PARAMETER_LIMIT,
-  limit: config.common.api.bodySizeLimit || DEFAULT_BODY_SIZE_LIMIT
-});
+  parameterLimit: Number(config.common.api.parameterLimit) || DEFAULT_PARAMETER_LIMIT
+};
 
 const app = express();
 
@@ -29,10 +29,10 @@ app.use(cors());
 app.use('/docs', express.static(path.join(__dirname, 'docs')));
 
 // Client must send "Content-Type: application/json" header
-app.use(bodyParser.json(bodyParserJsonConfig()));
-app.use(bodyParser.urlencoded(bodyParserUrlencodedConfig()));
-app.use(expressRequestIdMiddleware);
+app.use(bodyParser.json(bodyParserJsonConfig));
+app.use(bodyParser.urlencoded(bodyParserUrlencodedConfig));
 
+app.use(expressRequestIdMiddleware);
 if (!config.isTesting) {
   app.use(expressMiddleware);
 }
